75ea11f073f106b7be77e3926c975ca8864ed1ed
@hexlet/cli <support@hexlet.io>
2022-12-14 10:37:35 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -1,6 +1,6 @@
 package exercise;
 
-//import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
@@ -13,25 +13,20 @@ class AppTest {
     @Test
     void testTake() {
         // BEGIN
-        int count1 = 4;
-        List<Integer> numbers1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        Assertions.assertEquals(App.take(numbers1, count1).size(), 4);
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> expected1 = new ArrayList<>();
+        List<Integer> result1 = App.take(list1, 3);
+        assertThat(result1).isEqualTo(expected1);
 
-        int count2 = 0;
-        List<Integer> numbers2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        Assertions.assertEquals(App.take(numbers2, count2).size(), 0);
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        List<Integer> expected2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        List<Integer> result2 = App.take(list2, 10);
+        assertThat(result2).isEqualTo(expected2);
 
-        int count3 = 2;
-        List<Integer> numbers3 = new ArrayList<>();
-        Assertions.assertEquals(App.take(numbers3, count3).size(), 0);
-
-        int count4 = 2;
-        List<Integer> numbers4 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        Assertions.assertEquals(App.take(numbers4, count4).size(), 2);
-
-        int count5 = 2;
-        List<Integer> numbers5 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        Assertions.assertEquals(App.take(numbers5, count5).get(0), 1);
+        List<Integer> list3 = new ArrayList<>(Arrays.asList(1, 2, 3));
+        List<Integer> expected3 = new ArrayList<>(Arrays.asList(1, 2));
+        List<Integer> result3 = App.take(list3, 2);
+        assertThat(result3).isEqualTo(expected3);
         // END
     }
 }
