3e5c22da786fd10266534c6c50de773f819b9e3d
@hexlet/cli <support@hexlet.io>
2023-10-05 08:20:01 +0300

    submit java-spring-ru/error-handling

---
+++ java-spring-ru/error-handling/src/main/java/exercise/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package exercise.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import exercise.model.Product;
+
+// BEGIN
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>, CrudRepository<Product, Long> {
+
+}
+// END
