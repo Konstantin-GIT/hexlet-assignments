d73c656c4a5800301ae802621272e945fabd5587
@hexlet/cli <support@hexlet.io>
2023-10-11 12:27:46 +0300

    submit java-spring-ru/spring-lifecycle

--- java-spring-ru/spring-lifecycle/src/main/java/exercise/CustomBeanPostProcessor.java
+++ java-spring-ru/spring-lifecycle/src/main/java/exercise/CustomBeanPostProcessor.java
@@ -4,6 +4,29 @@ package exercise;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-// BEGIN
+import org.springframework.stereotype.Component;
 
+// BEGIN
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    // Метод вызывается перед инициализацией бина
+    // Принимает сам бин и имя бина
+    // Должен вернуть бин
+    public Object postProcessBeforeInitialization(Object bean, String beanName)
+        throws BeansException {
+        System.out.println("Called PostProcessBeforeInitialization: " + beanName);
+        return bean;
+    }
+
+    @Override
+    // Метод вызывается после инициализацией бина
+    // Должен вернуть бин
+    public Object postProcessAfterInitialization(Object bean, String beanName)
+        throws BeansException {
+        System.out.println("Called PostProcessAfterInitialization: " + beanName);
+        return bean;
+    }
+}
 // END
